1f4428e821b53ca837c5709c2845c0e7b94f6c45
execuu <hackedmind0613@gmail.com>
2025-08-23 18:15:49 +0800

    fix: cloudinary plz

--- backend/jpcs_backend/settings.py
+++ backend/jpcs_backend/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 # ENABLE MEDIA ROOT FOR DEVELOPMENT
 
